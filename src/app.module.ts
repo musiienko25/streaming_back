f183ff22f7f73766ab75483526ba1bbb8ba9d63c
musiienko25 <devmusiienko@gmail.com>
2025-01-24 16:32:39 +0200

    connected middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlowersModule } from './flowers/flowers.module';
 import { FlowersService } from './flowers/flowers.service';
+import { MiddlewareConsumer } from '@nestjs/common';
+import { BasicMiddleware } from './conception/middleware';
 
 @Module({
   imports: [FlowersModule],
   controllers: [AppController],
   providers: [AppService, FlowersService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(BasicMiddleware).forRoutes('flowers');
+  }
+}
